9e2ba706c74d7e89910d7d3d3cd31cb80732c1dc
ahmed1998aa <30708899+ahmed1998aa@users.noreply.github.com>
2017-09-09 11:06:38 -0700

    Update AboutMe.lua

--- plugins/AboutMe.lua
+++ plugins/AboutMe.lua
@@ -15,19 +15,19 @@ local function run(msg, matches)
   if matches[1] == 'me' then
     if is_sudo(msg) then
     send_document(get_receiver(msg), "./data/me/version.webp", ok_cb, false)
-      return "F A E D E R +\n---------------------------------------------\n|An Advanced Bot Based On #LUA|\n\n> Bot Number : +19092545429\n---------------------------------------------\n #Developer : [@Devphp_lua | @ll750kll_bot ]\n---------------------------------------------\n #Sudoer :\n1 >FAEDER \n---------------------------------------------\n> Channel : @Team_faeder"
+      return "V O D K A +\n---------------------------------------------\n|An Advanced Bot Based On #LUA|\n\n> Bot Number : +9647717463622\n---------------------------------------------\n #Developer : [@AHMED1998A | @U_U_I ]\n---------------------------------------------\n #Sudoer :\n1 >VODKA \n---------------------------------------------\n> Channel : @MASTER_OF_ART"
     elseif is_admin1(msg) then
     send_document(get_receiver(msg), "./data/me/admin.webp", ok_cb, false)
-      return "You're Admin"
+      return "You're Admin 👨‍💼❕"
     elseif is_owner(msg) then
     send_document(get_receiver(msg), "./data/me/owner.webp", ok_cb, false)
-      return "You're Owner"
+      return "You're Owner 👷❗️"
     elseif is_momod(msg) then
     send_document(get_receiver(msg), "./data/me/mod.webp", ok_cb, false)
-      return "You're Moderator"
+      return "You're Moderator 👨‍🎤❕"
     else
     send_document(get_receiver(msg), "./data/me/mmbr.webp", ok_cb, false)
-      return "You're Just member :("
+      return "You're Just member 👨‍💻❕+"
     end
   end
 end
